29e67e86745e4ae95e862a239a2e9d44994599ba
sakaguchi5 <56575533+sakaguchi5@users.noreply.github.com>
2026-05-08 16:57:55 +0900

    Import all major transition modules

--- CppFormalization/Cpp2/Closure/Transitions/Major/All.lean
+++ CppFormalization/Cpp2/Closure/Transitions/Major/All.lean
@@ -1,3 +1,10 @@
 import CppFormalization.Cpp2.Closure.Transitions.Scope.All
 import CppFormalization.Cpp2.Closure.Transitions.DeclareObject.All
 import CppFormalization.Cpp2.Closure.Transitions.DeclareRef.All
+import CppFormalization.Cpp2.Closure.Transitions.Major.DeclareRefStrengtheningConnection
+
+/-!
+# Cpp2.Closure.Transitions.Major.All
+
+Build-coverage aggregate for major transition connections.
+-/
