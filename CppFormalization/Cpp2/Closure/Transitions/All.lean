c281cd5fd474d39d4e1e6b45b22c801291807301
sakaguchi5 <56575533+sakaguchi5@users.noreply.github.com>
2026-05-08 17:26:47 +0900

    Import all transition aggregates

--- CppFormalization/Cpp2/Closure/Transitions/All.lean
+++ CppFormalization/Cpp2/Closure/Transitions/All.lean
@@ -2,3 +2,11 @@ import CppFormalization.Cpp2.Closure.Transitions.Assign.All
 import CppFormalization.Cpp2.Closure.Transitions.Scope.All
 import CppFormalization.Cpp2.Closure.Transitions.DeclareObject.All
 import CppFormalization.Cpp2.Closure.Transitions.DeclareRef.All
+import CppFormalization.Cpp2.Closure.Transitions.Major.All
+import CppFormalization.Cpp2.Closure.Transitions.Minor.All
+
+/-!
+# Cpp2.Closure.Transitions.All
+
+Build-coverage aggregate for all transition modules.
+-/
