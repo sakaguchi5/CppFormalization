bdc8c03de77ffb0d6c283bebac829366e1b590c5
sakaguchi5 <56575533+sakaguchi5@users.noreply.github.com>
2026-05-08 17:24:46 +0900

    Import all minor transition modules

--- CppFormalization/Cpp2/Closure/Transitions/Minor/All.lean
+++ CppFormalization/Cpp2/Closure/Transitions/Minor/All.lean
@@ -1,2 +1,9 @@
 import CppFormalization.Cpp2.Closure.Transitions.Assign.All
 import CppFormalization.Cpp2.Closure.Transitions.Scope.All
+import CppFormalization.Cpp2.Closure.Transitions.Minor.StateUpdateRoadmap
+
+/-!
+# Cpp2.Closure.Transitions.Minor.All
+
+Build-coverage aggregate for minor transition roadmap modules.
+-/
