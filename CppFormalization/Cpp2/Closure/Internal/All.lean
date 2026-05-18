f68cb2c08e31b52cc7a110110f312f618e625e52
sakaguchi5 <sokyu_yokyu@ezweb.ne.jp>
2026-05-18 21:41:03 +0900

    FunctionBodyCaseDriverContinuationIHCI.leanの追加
    lake build確認済み

--- CppFormalization/Cpp2/Closure/Internal/All.lean
+++ CppFormalization/Cpp2/Closure/Internal/All.lean
@@ -124,7 +124,7 @@ import CppFormalization.Cpp2.Closure.Internal.WhileTailReentryProviderSplitCI
 import CppFormalization.Cpp2.Closure.Internal.Transport.All
 import CppFormalization.Cpp2.Closure.Internal.SeqFunctionBodyClosureContinuationCoreSupportCI
 import CppFormalization.Cpp2.Closure.Internal.FunctionBodyCaseDriverContinuationSeqSupportCI
-
+import CppFormalization.Cpp2.Closure.Internal.FunctionBodyCaseDriverContinuationIHCI
 /-!
 # CppFormalization.Cpp2.Closure.Internal.All
 
