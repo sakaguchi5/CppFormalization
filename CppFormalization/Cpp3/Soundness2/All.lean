900af8822b501fdcfc8b8e8c6a446829c894a8d9
sakaguchi5 <sokyu_yokyu@ezweb.ne.jp>
2026-06-11 13:30:00 +0900

    Soundness2の見直し3
    Derive.ClosedInternal以外を削除
    lake build 確認済み

--- CppFormalization/Cpp3/Soundness2/All.lean
+++ CppFormalization/Cpp3/Soundness2/All.lean
@@ -1,4 +1,4 @@
 import CppFormalization.Cpp3.Soundness2.Source.All
 import CppFormalization.Cpp3.Soundness2.Realize.All
-import CppFormalization.Cpp3.Soundness2.Derive.All
+import CppFormalization.Cpp3.Soundness2.Derive.ClosedInternal
 import CppFormalization.Cpp3.Soundness2.Final.All
--- CppFormalization/Cpp3/Soundness2/Derive/All.lean
+++
@@ -1,5 +0,0 @@
-import CppFormalization.Cpp3.Soundness2.Derive.Boundary
-import CppFormalization.Cpp3.Soundness2.Derive.LocalControl
-import CppFormalization.Cpp3.Soundness2.Derive.ScopeExit
-import CppFormalization.Cpp3.Soundness2.Derive.LoopEngine
-import CppFormalization.Cpp3.Soundness2.Derive.ClosedInternal
